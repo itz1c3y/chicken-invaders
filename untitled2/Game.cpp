cff0f6b8573ba0e7d3c1a8adc56cd62ce48f954f
itz1c3y <90198973+itz1c3y@users.noreply.github.com>
2024-07-02 11:42:08 +0330

    Add files via upload

---
+++ untitled2/Game.cpp
@@ -0,0 +1,39 @@
+#include "Game.h"
+#include <QApplication>
+#include "MyRect.h"
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QTimer>
+
+
+// create a scene
+auto * scene = new QGraphicsScene();
+
+// create an item to add to the scene
+auto * player = new MyRect();
+player->setRect(0,0,100,100); // change the rect from 0x0 (default) to 100x100 pixels
+
+// add the item to the scene
+scene->addItem(player);
+
+// make rect focusable
+player->setFlag(QGraphicsItem::ItemIsFocusable);
+player->setFocus();
+
+// create a view to visualize the scene
+auto * view = new QGraphicsView(scene);
+view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+// show the view
+view->show();
+view->setFixedSize(800,600);
+scene->setSceneRect(0,0,800,600);
+
+player->setPos(view->width()/2,view->height() - player->rect().height());
+
+// spawn enemies
+auto * timer = new QTimer();
+QObject::connect(timer,SIGNAL(timeout()),player,SLOT(spawn()));
+timer->start(2000);
+
---
+++ untitled2/Game.h
@@ -0,0 +1,12 @@
+
+
+#ifndef UNTITLED2_GAME_H
+#define UNTITLED2_GAME_H
+
+
+class Game {
+
+};
+
+
+#endif //UNTITLED2_GAME_H
